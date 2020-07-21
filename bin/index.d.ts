d8531ed7a502c8bf37aab4d3c381b5c9d8d04261
MasatoMakino <unagiinu.g@gmail.com>
2020-07-21 21:09:37 +0900

    change : bin to lib.

--- bin/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { SliderEvent, SliderEventType } from "./SliderEvent";
-export { SliderView } from "./SliderView";
-export { ScrollBarView, ScrollBarViewInitOption } from "./ScrollBarView";
-export { SliderViewOption } from "./SliderViewOption";
-//# sourceMappingURL=index.d.ts.map
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,4 @@ export { SliderEvent, SliderEventType } from "./SliderEvent";
 export { SliderView } from "./SliderView";
 export { ScrollBarView, ScrollBarViewInitOption } from "./ScrollBarView";
 export { SliderViewOption } from "./SliderViewOption";
+//# sourceMappingURL=index.d.ts.map
